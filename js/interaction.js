0acb79614f40e06fd9a5a37040916bba48feaf6d
Agathe <molleagathe@gmail.com>
2013-10-20 13:41:18 +0200

    battery level

--- js/interaction.js
+++ js/interaction.js
@@ -3,8 +3,19 @@
 	jQuery(".light .button img").on("click",function(){
 		jQuery(".light").toggleClass("active");
 
-
-
 	})
+	batteryChange(15);
 
 }(jQuery));
+
+function batteryChange(batteryLevel) {
+	var inverseLevel = 100-batteryLevel;
+	document.getElementById('batteryLevel').style.top = inverseLevel+"%";
+	if(batteryLevel >= 20) {
+		document.getElementById('batteryLevel').style.backgroundColor = "#D5D24D";
+	}
+	else {
+		document.getElementById('batteryLevel').style.backgroundColor = "#E95B57";
+	}
+	document.getElementById('batteryValue').innerHTML=batteryLevel+"<span>%</span>";
+}
